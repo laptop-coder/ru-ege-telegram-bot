70240b99a5459b41a8d75ff7be29205004ce95dc
laptop-coder <coderlaptop@gmail.com>
2025-09-16 22:48:39 +0300

    feat: add database module

---
+++ src/db.py
@@ -0,0 +1,23 @@
+import os
+import sqlite3
+
+import config
+from config import logger
+
+
+init_is_needed = False
+
+if not os.path.exists(config.PATH_TO_DB):
+    logger.info("Database file not found, initial SQL queries will be executed...")
+    init_is_needed = True
+else:
+    logger.info("Found database file")
+
+connection = sqlite3.connect(config.PATH_TO_DB)
+
+if init_is_needed:
+    with connection:
+        cursor = connection.cursor()
+        with open("/bot/src/db.sql", "r") as file:
+            sql_queries = file.read()
+            cursor.executescript(sql_queries)
