b99c3b543349a41ed310087606523da0a826dfd0
laptop-coder <coderlaptop@gmail.com>
2025-09-17 01:15:02 +0300

    feat: add `Category` enum

---
+++ src/enums.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Category(Enum):
+    orthoepy = "orthoepy"
+    paronyms = "paronyms"
+    phraseological_unit = "phraseological_unit"
+    unstressed_at_root = "unstressed_at_root"
